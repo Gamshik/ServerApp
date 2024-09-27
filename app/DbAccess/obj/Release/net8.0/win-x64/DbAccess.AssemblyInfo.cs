d1a21d00a740c2e648b1f7b2bfafd2164123d3e7
Gamshik <glebovich157rtyp@gmail.com>
2024-09-27 08:09:42 +0000

    Update builds

--- app/DbAccess/obj/Release/net8.0/win-x64/DbAccess.AssemblyInfo.cs
+++ app/DbAccess/obj/Release/net8.0/win-x64/DbAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0f2953ab9e52522e5f646e8df7f78784d21a7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a90bb16aa01cb38638668a5319dd5574ad7fb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
