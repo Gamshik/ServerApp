11a90bb16aa01cb38638668a5319dd5574ad7fb0
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-09-27 11:07:46 +0300

    Update proj

--- app/DbAccess/obj/Debug/net8.0/DbAccess.AssemblyInfo.cs
+++ app/DbAccess/obj/Debug/net8.0/DbAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25f7e3bc24dbd47602ce1b7e95b16ecc1e3ccab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0f2953ab9e52522e5f646e8df7f78784d21a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
